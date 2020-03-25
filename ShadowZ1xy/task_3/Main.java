e0151d784ca3172b715c5f7f2e0207d84011aa31
ShadowZ1xy <shadow.z1xy@gmail.com>
2020-03-25 15:11:21 +0400

    Add directory Add temp classes for tasks

---
+++ ShadowZ1xy/task_3/Main.java
@@ -0,0 +1,7 @@
+package ShadowZ1xy.task_3;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("temp3");
+    }
+}
---
+++ ShadowZ1xy/task_4/Main.java
@@ -0,0 +1,7 @@
+package ShadowZ1xy.task_4;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("temp4");
+    }
+}
